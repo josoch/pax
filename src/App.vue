ed52b2f5ff7e621cd6673266f0838eb96b68f888
Josoch <ocholijoseph@gmail.com>
2024-05-30 12:27:49 +0000

    Solving problems

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,11 @@
 <template>
-  <div class="container mt-5">
-    <ul class="nav nav-tabs" id="myTab" role="tablist">
-      <li class="nav-item" role="presentation">
-        <button class="nav-link active" id="sign-in-tab" data-bs-toggle="tab" data-bs-target="#sign-in" type="button" role="tab" aria-controls="sign-in" aria-selected="true">Sign In</button>
-      </li>
-      <li class="nav-item" role="presentation">
-        <button class="nav-link" id="sign-up-tab" data-bs-toggle="tab" data-bs-target="#sign-up" type="button" role="tab" aria-controls="sign-up" aria-selected="false">Sign Up</button>
-      </li>
-    </ul>
-    <div class="tab-content" id="myTabContent">
-      <div class="tab-pane fade show active" id="sign-in" role="tabpanel" aria-labelledby="sign-in-tab">
-        <SignIn />
-      </div>
-      <div class="tab-pane fade" id="sign-up" role="tabpanel" aria-labelledby="sign-up-tab">
-        <SignUp />
-      </div>
-    </div>
-    <router-view></router-view> <!-- Ensure this is included for routing -->
+  <div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import SignIn from './components/SignInSignUp.vue';
-import SignUp from './components/SignUp.vue';
-
 export default {
-  name: 'App',
-  components: {
-    SignIn,
-    SignUp
-  }
+  name: 'App'
 };
 </script>
