f1a30ad580b0b6cdcd4380b110d97ef089e0bddb
Josoch <ocholijoseph@gmail.com>
2024-06-01 10:36:48 +0000

    Logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">
-            <img src="@/assets/logo.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
+            <img src="./assets/logo.svg" alt="Logo" width="30" height="30" class="d-inline-block align-text-centre">
             FamilyApp
           </a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
